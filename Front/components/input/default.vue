07af275edabe2bc101764ddefd6b2a6e81ca9f39
t-coffe <tcoffe@edenschool.fr>
2025-03-19 16:26:56 +0100

    Affichage du résultat après la validation

--- Front/components/input/default.vue
+++ Front/components/input/default.vue
@@ -1,18 +1,21 @@
 <template>
-    <li class="ring-0 ring-inset ring-highlight-medium p-3 shadow-sm rounded-xl bg-bg-primary"  :class="selectedReponse === reponse.id_reponse ? 'ring-4' : 'ring-0'">
-        <input type="radio" class="hidden" name="reponse" :id="reponse.id_reponse" :value="reponse.id_reponse" @click="updateSelectedReponse(reponse.id_reponse)">
-        <label :for="reponse.id_reponse" class="cursor-pointer flex items-center gap-4">
-            <span class="w-9 h-9 flex justify-center items-center bg-bg-secondary rounded-md ">{{ letter }}</span>
-            <span>{{ reponse.label }}</span>
-        </label>
+    <li class="ring-0 ring-inset p-3 shadow-sm rounded-xl bg-bg-primary"  :class="{
+        'ring-4 ring-highlight-medium': selectedReponse === reponse.id_reponse && validatedReponse === null,
+        'ring-4 ring-support-error-medium': selectedReponse === reponse.id_reponse && validatedReponse === true,
+        'ring-4 ring-support-success-medium': selectedReponse === reponse.id_reponse && validatedReponse === false,
+        'ring-0': selectedReponse !== reponse.id_reponse
+    }">
+        <input type="radio" class="invisible" name="reponse" :id="reponse.id_reponse" :value="reponse.id_reponse" @click="updateSelectedReponse(reponse.id_reponse)">
+        <label :for="reponse.id_reponse" class="cursor-pointer" >{{ reponse.label }}</label>
     </li>
 </template>
-
+// TODO: Fix la couleur de bordure marche que la première condition
 <script setup>
     const props = defineProps({
         reponse: Object,
         selectedReponse: String,
-        updateSelectedReponse: Function,
-        letter: String
+        updateSelectedReponse: Function
     });
+
+    console.log(props.validatedReponse.value)
 </script>
