0d658e4e1b3eb0894a60ebad5a15e0e2400c657a
IronLegends7519 <drafin@edenschool.fr>
2025-03-21 12:17:29 +0100

    login css et adapte maquette

--- Back/database/migrations/0001_01_01_000000_user.php
+++ Back/database/migrations/0001_01_01_000000_user.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user', function (Blueprint $table) {
             $table->string('user_id')->primary();
             $table->string('email')->unique();
+            $table->string('username')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('role_id');
