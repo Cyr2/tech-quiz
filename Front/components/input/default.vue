d84b7820016e82dd6506dcab22f2e9a329bb2d51
Cyr2 <cyrigamingpro@gmail.com>
2025-03-21 14:59:00 +0100

    Ajout des bordures et pastilles celon le résultat

--- Front/components/input/default.vue
+++ Front/components/input/default.vue
@@ -1,25 +1,33 @@
 <template>
-    <li class="ring-0 ring-inset p-3 shadow-sm rounded-xl bg-bg-primary"  :class="{
-        'ring-4 ring-highlight-medium': selectedReponse === reponse.id_reponse && validatedReponse === null,
-        'ring-4 ring-support-error-medium': selectedReponse === reponse.id_reponse && validatedReponse === true,
-        'ring-4 ring-support-success-medium': selectedReponse === reponse.id_reponse && validatedReponse === false,
-        'ring-0': selectedReponse !== reponse.id_reponse
-    }">
-        <input type="radio" class="invisible" name="reponse" :id="reponse.id_reponse" :value="reponse.id_reponse" @click="updateSelectedReponse(reponse.id_reponse)">
+    <li class="relative ring-4 ring-inset p-3 shadow-sm rounded-xl bg-bg-primary flex items-center justify-between" :class="ringColor">
+        <input type="radio" class="opacity-0 absolute w-full h-full top-0 left-0 z-10 cursor-pointer" name="reponse" :id="reponse.id_reponse" :value="reponse.id_reponse" @click="updateSelectedReponse(reponse.id_reponse)">
         <label :for="reponse.id_reponse" class="cursor-pointer" >{{ reponse.label }}</label>
+        <div v-if="correctReponse !== reponse.id_reponse && validatedReponse === false && selectedReponse === reponse.id_reponse" class="w-4 h-4 bg-support-error-medium rounded-full" />
+
+        <div v-if="correctReponse === reponse.id_reponse && validatedReponse !== null" class="w-4 h-4 bg-support-success-medium rounded-full" />
     </li>
 </template>
-// TODO: Fix, la couleur de bordure marche que la première condition.
-// TODO: Fix, la couleur de bordure marche que la première condition.
 <script setup>
-    const props = defineProps({
-        reponse: Object,
-        selectedReponse: String,
-        updateSelectedReponse: Function,
-        validatedReponse: Boolean | null
-    });
+import { computed } from 'vue';
 
-    console.log(props.validatedReponse.value)
+const props = defineProps({
+    reponse: Object,
+    selectedReponse: String,
+    updateSelectedReponse: Function,
+    validatedReponse: Boolean | null,
+    correctReponse: String | null
+});
 
-    console.log(props.validatedReponse.value)
+const ringColor = computed(() => {
+    console.log(props.selectedReponse, props.reponse.id_reponse, props.validatedReponse);
+    
+    if (props.selectedReponse === props.reponse.id_reponse && props.validatedReponse === null) {
+        return 'ring-4 ring-highlight-medium';
+    } else if (props.selectedReponse === props.reponse.id_reponse && props.validatedReponse === true) {
+        return 'ring-4 ring-support-success-medium';
+    } else if (props.selectedReponse === props.reponse.id_reponse && props.validatedReponse === false) {
+        return 'ring-4 ring-support-error-medium';
+    }
+    return '!ring-0';
+});
 </script>
