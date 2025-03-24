5e5fdd57fea3ef51f5d52489fd65eb20f9a74cd2
t-coffe <tcoffe@edenschool.fr>
2025-03-24 16:22:51 +0100

    composant tableau admin

---
+++ Front/pages/admin/users/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Users</h1>
+    <AdminTable v-if="data" :data="data" />
+</template>
+
+<script setup>
+    import { fetchUsers } from "../../../utils/fetchUsers";
+
+    const data = ref(null);
+
+    onMounted(async () => {
+        data.value = await fetchUsers();
+    });
+</script>
