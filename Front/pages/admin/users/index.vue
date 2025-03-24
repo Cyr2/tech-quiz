91d73a6c0321f1f50223e8dec79ddadec3628183
t-coffe <tcoffe@edenschool.fr>
2025-03-24 17:26:13 +0100

    Admin | css affichage utilisateur

--- Front/pages/admin/users/index.vue
+++ Front/pages/admin/users/index.vue
@@ -1,6 +1,11 @@
 <template>
-    <h1>Users</h1>
-    <AdminTable v-if="data" :data="data" />
+    <div class="flex flex-col items-center gap-6">
+        <div class="flex justify-between w-full">
+            <h1 class="">Users</h1>
+            <NuxtLink to="/admin/users/create" class="btn btn-primary bg-support-success-medium rounded-md text-bg-primary px-2 py-1">Create</NuxtLink>
+        </div>
+        <AdminTable v-if="data" :data="data" />
+    </div>
 </template>
 
 <script setup>
