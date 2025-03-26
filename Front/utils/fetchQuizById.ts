ac6afc032f07e6d7c317be5f46d2163cf99616dd
lroux07 <lroux@edenschool.fr>
2025-03-26 11:40:54 +0100

    debut de recuperer en bdd

---
+++ Front/utils/fetchQuizById.ts
@@ -0,0 +1,17 @@
+import { useAuth } from '../stores/auth.js';
+
+export async function fetchQuizById(id: number) {
+    const token  = useAuth();
+    const response = await fetch(`http://127.0.0.1:8000/api/quiz/{id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization':'Bearer ' + token.getToken()
+        }
+    });
+
+    const data = await response.json();
+
+    return data;
+
+}
