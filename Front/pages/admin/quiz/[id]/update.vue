b7b05365240396d5b17ee9034b229f2df5293f2d
lroux07 <lroux@edenschool.fr>
2025-03-26 11:40:54 +0100

    debut de recuperer en bdd

--- Front/pages/admin/quiz/[id]/update.vue
+++ Front/pages/admin/quiz/[id]/update.vue
@@ -4,47 +4,51 @@
             <label for="title" class="font-bold text-yellow-500">Titre</label>
             <button class="text-red-400">Supprimer le quiz</button>
         </div>
-        <input type="text" id="title" name="title" class="border-2 border-solid border-solid p-2 rounded-md" value="Ceci est un quiz">
+        <input type="text" id="title" name="title" class="border-2 border-solid border-solid p-2 rounded-md" :value="tab.title">
 
         <div class="bg-white rounded-lg p-6 shadow-sm flex flex-col gap-8 w-96 border-2 border-solid w-full">
             <div class="flex flex-col gap-2">
                 <div class="flex flex-row justify-between">
                     <label for="question" class="font-bold">Question {{ currentQuestion + 1 }}</label>
-                    <button class="text-red-400">Supprimer la question</button>
+                    <button class="text-red-400" @click.prevent="removeQuestion(currentQuestion)">Supprimer la question</button>
                 </div>
-                <input type="text" id="question" name="question" class="border-2 border-solid p-2" :value="tab[currentQuestion].question">
+                <input type="text" id="question" name="question" class="border-2 border-solid p-2" v-model="tab.questions[currentQuestion].label">
 
-                <div class="flex flex-row justify-between">
-                    <label for="answere1">Réponse 1</label>
-                    <button class="text-red-400">Supprimer la réponse</button>
+                <div v-for="(answer, index) in tab.questions[currentQuestion].answers" :key="index" class="flex flex-col gap-2">
+                    <div class="flex flex-row justify-between">
+                        <label :for="'answer' + index">Réponse {{ index + 1 }}</label>
+                        <button class="text-red-400" @click.prevent="removeAnswer(currentQuestion, index)">Supprimer la réponse</button>
+                    </div>
+                    <div class="flex flex-col items-start gap-2">
+                        <input type="text" :id="'answer' + index" :name="'answer' + index" class="border-2 border-solid p-2 w-full" v-model="answer.label">
+                        <div class="flex gap-4 mb-4" v-if="answer.isCorrect">
+                            <label :for="'isCorrect' + index">Réponse correcte</label>
+                            <input type="radio" name="isCorrect" :id="'isCorrect' + index" checked>
+                        </div>
+                        <div class="flex gap-4 mb-4" v-else>
+                            <label :for="'isCorrect' + index">Réponse correcte</label>
+                            <input type="radio" name="isCorrect" :id="'isCorrect' + index">
+                        </div>
+                    </div>
                 </div>
-                <input type="text" id="answere1" name="answere1" class="border-2 border-solid p-2" :value="tab[currentQuestion].reponses[0]">
-                
-                <div class="flex flex-row justify-between">
-                    <label for="answere1">Réponse 2</label>
-                    <button class="text-red-400">Supprimer la réponse</button>
-                </div>
-                <input type="text" id="answere2" name="answere2" class="border-2 border-solid p-2" :value="tab[currentQuestion].reponses[1]">
 
-                <div class="flex flex-row justify-between">
-                    <label for="answere1">Réponse 3</label>
-                    <button class="text-red-400">Supprimer la réponse</button>
-                </div>
-                <input type="text" id="answere3" name="answere3" class="border-2 border-solid p-2" :value="tab[currentQuestion].reponses[2]">
-
-                <div class="flex flex-row justify-between">
-                    <label for="answere1">Réponse 1</label>
-                    <button class="text-red-400">Supprimer la réponse</button>
-                </div>
-                <input type="text" id="answere4" name="answere4" class="border-2 border-solid p-2" :value="tab[currentQuestion].reponses[3]">
-
-                <button class="flex text-gray-400 mt-2">+ Ajouter une réponse</button>
-                
-                <!-- Ajouter un bouton pour ajouter des réponses -->
-                <!-- Si on est sur la derniere question mettre un bouton ajouter une question à la place de suivant -->
+                <button class="flex text-gray-400 mt-2" @click.prevent="addAnswer(currentQuestion)">+ Ajouter une réponse</button>
 
                 <div class="flex flex-row gap-8 justify-between mt-6">
-                    <button @click.prevent="prevQuestion" class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">Precedent</button>
+                    <button 
+                        v-if="currentQuestion === 0"
+                        @click.prevent="prevQuestion" 
+                        class="bg-gray-200 p-6 rounded-lg h-8 w-96 flex items-center justify-center"
+                        disabled="true">
+                        Precedent
+                    </button>
+                    <button 
+                        v-else
+                        @click.prevent="prevQuestion" 
+                        class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">
+                        Precedent
+                    </button>
+
                     <button
                         v-if="!LastQuestion" 
                         @click.prevent="nextQuestion" 
@@ -52,7 +56,8 @@
                         Suivant
                     </button>
                     <button 
-                        v-if="LastQuestion" 
+                        v-else
+                        @click.prevent="addQuestion"
                         class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">
                         Ajouter une question
                     </button>
@@ -65,34 +70,9 @@
 </template>
 
 <script setup>
-const tab = [
-    {
-        question: "Comment ça va ?",
-        reponses: [
-            "Bien",
-            "Mal",
-            "Oui",
-            "Toi je t'aime pas"
-        ]
-    },
-    {
-        question: "Comment tu t'appelles ?",
-        reponses: [
-            "Jean",
-            "Paul",
-            "Jacques",
-            "Marie"
-        ]
-    }
-];
-
-const test = 2;
-
-const LastQuestion = computed(() => currentQuestion.value === tab.length - 1);
-
 import { ref, computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
-import { fetchQuizById } from '../../../../utils/fetchQuiz';
+import { fetchQuizById } from '../../../../utils/fetchQuizById';
 import { fetchQuestions } from '../../../../utils/fetchQuestions';
 import { fetchReponses } from '../../../../utils/fetchReponses';
 
@@ -100,28 +80,83 @@ const route = useRoute();
 
 const id = computed(() => route.params.id);
 
-const quiz = ref(null);
+
+const tab = ref({
+    title: 'Ceci est un nom de quiz',
+    questions: [
+        {
+            label: 'Comment ça va ?',
+            answers: [
+                { label: 'Bien', isCorrect: "on" },
+                { label: 'Mal', isCorrect: false },
+                { label: 'Oui', isCorrect: false },
+                { label: 'Toi je t\'aime pas', isCorrect: false }
+            ],
+        },
+        {
+            label: 'Comment tu t\'appelles ?',
+            answers: [
+                { label: 'Jean', isCorrect: false },
+                { label: 'Paul', isCorrect: false },
+                { label: 'Jacques', isCorrect: false },
+                { label: 'Je sais pas', isCorrect: "on" }
+            ],
+        }
+    ],
+});
+
 const currentQuestion = ref(0);
 
+const LastQuestion = computed(() => currentQuestion.value === tab.value.questions.length - 1);
+
 const nextQuestion = () => {
-    if (currentQuestion.value < tab.length - 1) {
+    if (currentQuestion.value < tab.value.questions.length - 1) {
         currentQuestion.value++;
     }
 };
 
 const prevQuestion = () => {
-    if (currentQuestion.value < tab.length + 1) {
+    if (currentQuestion.value > 0) {
         currentQuestion.value--;
     }
-}
+};
+
+const addQuestion = () => {
+    tab.value.questions.push({
+        label: 'Nouvelle question ?',
+        answers: [
+            { label: '', isCorrect: false },
+            { label: '', isCorrect: false },
+        ],
+    });
+    currentQuestion.value = tab.value.questions.length - 1;
+};
+
+const removeQuestion = (index) => {
+    if (tab.value.questions.length > 1) {
+        tab.value.questions.splice(index, 1);
+        currentQuestion.value = Math.max(0, currentQuestion.value - 1);
+    }
+};
+
+const addAnswer = (questionIndex) => {
+    if(tab.value.questions[questionIndex].answers.length >= 4) return;
+    tab.value.questions[questionIndex].answers.push({ label: '', isCorrect: false });
+};
+
+const removeAnswer = (questionIndex, answerIndex) => {
+    if (tab.value.questions[questionIndex].answers.length > 2) {
+        tab.value.questions[questionIndex].answers.splice(answerIndex, 1);
+    }
+};
 
 onMounted(async () => {
     try {
-        quiz.value = await fetchQuizById(id.value);
-        questions.value = await fetchQuestions(id.value);
-        if (questions.value && questions.value.length > 0) {
-            reponses.value = await fetchReponses(questions.value[currentQuestion.value].id_question);
-        }
+        const quizData = await fetchQuizById(id.value);
+        // if (quizData) {
+        //     tab.value = quizData;
+        // }
+        console.log(quizData);
     } catch (error) {
         console.error(error);
     }
