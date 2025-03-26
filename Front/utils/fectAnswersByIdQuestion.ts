c19bb210b74d4805f86b72719a0c9d214900e10f
lroux07 <lroux@edenschool.fr>
2025-03-26 15:18:00 +0100

    En cours d'enregistrement

--- Front/utils/fectAnswersByIdQuestion.ts
+++ Front/utils/fectAnswersByIdQuestion.ts
@@ -1,15 +1,15 @@
 import { useAuth } from '../stores/auth.js';
 
-export async function fetchQuizById(id: number) {
+export async function fetchAnswersByIdQuestion(id: number) {
     const token  = useAuth();
-    const response = await fetch(`http://127.0.0.1:8000/api/quiz/${id}`, {
+    const response = await fetch(`http://127.0.0.1:8000/api/answer/question/${id}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization':'Bearer ' + token.getToken()
         }
     });
-
+    
     const data = await response.json();
 
     return data;
