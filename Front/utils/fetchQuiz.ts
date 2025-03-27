9e2b1d2d3ffc5f61699adf76503bd0d5e3dd6d66
IronLegends7519 <drafin@edenschool.fr>
2025-03-27 17:21:44 +0100

    rajout de la navigation

--- Front/utils/fetchQuiz.ts
+++ Front/utils/fetchQuiz.ts
@@ -2,10 +2,8 @@ import { useAuth } from "../stores/auth.js";
 
 /* fonction permettant de recupérer un quiz */
 export async function fetchQuiz(quiz_id: string) {
-export async function fetchQuiz() {
     const token  = useAuth();
-    const response = await fetch(`http://127.0.0.1:8000/api/quiz`, {
-    const response = await fetch(`http://127.0.0.1:8000/api/quiz`, {
+    const response = await fetch(`http://127.0.0.1:8000/api/quiz/${quiz_id}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
