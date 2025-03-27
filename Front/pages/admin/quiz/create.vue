9a3fc4333c67620f5a1210904dbd4ad194dfd9a8
lroux07 <lroux@edenschool.fr>
2025-03-27 14:05:09 +0100

    Modification du front

--- Front/pages/admin/quiz/create.vue
+++ Front/pages/admin/quiz/create.vue
@@ -1,45 +1,57 @@
 <template>
-    <section class="bg-white rounded-lg p-10 w-96 flex flex-col gap-4 shadow-xl">
-        <article class="flex flex-col gap-2">
-            <label for="title">Title</label>
-            <input type="text" id="title" name="title" class="border-2 border-solid" v-model="quiz.title" @input="verifQuiz" />
-            <p v-if="quiz.errorQuiz"> {{ quiz.errorQuiz }} </p>
-        </article>
-        <article>
-            <ul v-if="quiz.questions.length" class="flex flex-col gap-4">
-                <li v-for="(question, index) in quiz.questions" :key="index" class="border-2 border-solid p-4 flex flex-col gap-6">
-                    <article class="flex flex-col gap-2">
-                        <label for="question">Question</label>
-                        <input type="text" id="question" name="question" class="border-2 border-solid" v-model="quiz.questions[index].label" @input="verifQuestion(question)" />
-                        <p v-if="quiz.questions[index].errorQuestion"> {{ quiz.questions[index].errorQuestion }} </p> 
-                    </article>
-                    <ul class="flex flex-col gap-4">
-                        <li v-for="(answer, indexAnswer) in quiz.questions[index].answers" :key="indexAnswer" class="border-2 border-solid p-4">
-                            <article class="flex flex-col gap-2">
-                                <label for="answer">Answer</label>
-                                <input type="text" id="answer" name="answer" class="border-2 border-solid" v-model="quiz.questions[index].answers[indexAnswer].label" @input="verifAnswer(answer)" />
-                                <p v-if="quiz.questions[index].answers[indexAnswer].errorAnswer"> {{ quiz.questions[index].answers[indexAnswer].errorAnswer }} </p>
-                            </article>
-                            <article class="flex items-center gap-2">
-                                <label for="isCorrect">Is correct</label>
-                                <input type="radio" :name="index" id="isCorrect" name="isCorrect" class="border-2 border-solid" v-model="quiz.questions[index].answers[indexAnswer].isCorrect" @click="verifAnswerCorrect(answer)" />
-                            </article>
-                        </li>
-                    </ul> 
-                    <ButtonDefaultForm class="mt-2" @click="addAnswer(index)">Ajouter une réponse</ButtonDefaultForm>
-                </li>
-            </ul>
-        </article>
-        <ButtonDefaultForm class="mt-2" @click="addQuestion">Ajouter une question</ButtonDefaultForm>
-        <ButtonDefaultForm class="mt-2" @click="addQuiz">Créer le quiz</ButtonDefaultForm>
-    </section> 
+    <form action="/" method="post" class="flex flex-col gap-4 w-2/4">
+        <div class="flex flex-row justify-between">
+            <label for="title" class="font-bold text-yellow-500">Titre</label>
+        </div>
+        <input type="text" id="title" name="title" class="border-2 border-solid border-solid p-2 rounded-md" placeholder="Ecrire ici">
+
+        <div class="bg-white rounded-lg p-6 shadow-sm flex flex-col gap-8 w-96 border-2 border-solid w-full">
+            <div class="flex flex-col gap-2">
+                <div class="flex flex-row justify-between">
+                    <label for="question" class="font-bold">Question 1</label>
+                    <button class="text-red-400" @click.prevent="removeQuestion">Supprimer la question</button>
+                </div>
+                <input type="text" id="question" name="question" class="border-2 border-solid p-2" placeholder="Ecrire ici">
+
+                <div class="flex flex-row justify-between">
+                    <label for="answer">Réponse 1</label>
+                    <button class="text-red-400" @click.prevent="removeAnswer">Supprimer la réponse</button>
+                </div>
+                <div class="flex flex-col items-start gap-2">
+                    <input type="text" id="answer" name="answer" class="border-2 border-solid p-2 w-full" placeholder="Ecrire ici">
+                    <div class="flex gap-4 mb-4">
+                        <input type="radio" name="isCorrect" id="isCorrect">
+                        <label for="isCorrect">Réponse correcte</label>
+                    </div>
+                </div>
+
+                <button class="flex text-gray-400 mt-2" @click.prevent="addAnswer">+ Ajouter une réponse</button>
+
+                <div class="flex flex-row gap-8 justify-between mt-6">
+                    <NuxtLink to="/"
+                        class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">
+                        Annuler
+                    </NuxtLink>
+                    <button
+                        @click.prevent="addQuestion"
+                        class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">
+                        Ajouter une question
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <button 
+            type="submit" 
+            class="bg-white border-2 border-solid hover:border-yellow-400 py-2 rounded-lg hover:bg-yellow-400 hover:text-black">
+            Enregistrer</button>
+    </form>
 </template>
- 
+
 <script setup>
 
 import { fetchCreateQuiz } from '../../../utils/fetchCreateQuiz';
 
-const errorQuiz = ref('');
 
 const quiz = ref({
     title: '',
@@ -47,13 +59,11 @@ const quiz = ref({
         {
             label: '',
             answers: [
-                { label: '', isCorrect: false, errorAnswer: '' },
-                { label: '', isCorrect: false, errorAnswer: '' },
-            ],
-            errorQuestion: ''
+                { label: '', isCorrect: false },
+                { label: '', isCorrect: false },
+            ]
         }
-    ],
-    errorQuiz: ''
+    ]
 });
 
 const addQuestion = () => {
@@ -66,72 +76,7 @@ const addQuestion = () => {
     });
 }
 
-const verifQuiz = () => {
-    if (quiz.value.title.trim() === '') {
-        quiz.value.errorQuiz = 'Le titre ne peut pas être vide';
-    } else {
-        quiz.value.errorQuiz = '';
-    }
-}
-
-const verifQuestion = (question) => {
-    if (question.label.trim() === '') {
-        question.errorQuestion = 'La question ne peut pas être vide';
-    } else {
-        question.errorQuestion = '';
-    }
-}
-
-const verifAnswer = (answer) => {
-    if (answer.label.trim() === '') {
-        answer.errorAnswer = 'La réponse ne peut pas être vide';
-    } else {
-        answer.errorAnswer = '';
-    }
-}
-
-const verifAnswerCorrect = (answer) => {
-    answer.errorAnswer = '';
-}
-
 const addQuiz = async () => {
-    if (quiz.value.errorQuiz !== '' || quiz.value.questions.some(question => question.errorQuestion !== '' || question.answers.some(answer => answer.errorAnswer !== ''))) {
-        console.log('error');
-        return;
-    }
-    if (quiz.value.title.trim() === '') {
-        quiz.value.errorQuiz = 'Le titre ne peut pas être vide';
-        return;
-    }
-    if (quiz.value.questions.some(question => question.label.trim() === '')) {
-        quiz.value.questions.forEach(question => {
-            if (question.label.trim() === '') {
-                question.errorQuestion = 'La question ne peut pas être vide';
-            }
-        });
-        return;
-    }
-    if (quiz.value.questions.some(question => question.answers.some(answer => answer.label.trim() === ''))) {
-        quiz.value.questions.forEach(question => {
-            question.answers.forEach(answer => {
-                if (answer.label.trim() === '') {
-                    answer.errorAnswer = 'La réponse ne peut pas être vide';
-                }
-            });
-        });
-        return;
-    }
-    if (quiz.value.questions.some(question => question.answers.filter(answer => answer.isCorrect).length === 0)) {
-        quiz.value.questions.forEach(question => {
-            if (question.answers.filter(answer => answer.isCorrect).length === 0) {
-                question.answers.forEach(answer => {
-                    answer.errorAnswer = 'Il doit y avoir au moins une réponse correcte';
-                });
-            } 
-        });
-        return;
-    } 
-
     const transformedQuiz = {
         ...quiz.value,
         questions: quiz.value.questions.map(question => ({
@@ -144,10 +89,7 @@ const addQuiz = async () => {
     };
     console.log(transformedQuiz);
     const data = await fetchCreateQuiz(transformedQuiz);
-    
-    if (data.sucess) {
-        navigateTo('/');  
-    }
+    console.log(data);    
 }
 
 const addAnswer = (questionIndex) => {
