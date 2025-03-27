109c4ffa2feef8b3b413c1d852ccffad7aca56f9
lroux07 <lroux@edenschool.fr>
2025-03-27 16:01:16 +0100

    Fin front et logique de la page de création de quiz

--- Front/pages/admin/quiz/create.vue
+++ Front/pages/admin/quiz/create.vue
@@ -9,23 +9,37 @@
             <div class="flex flex-col gap-2">
                 <div class="flex flex-row justify-between">
                     <label :for="'question-' + questionIndex" class="font-bold">Question {{ questionIndex + 1 }}</label>
-                    <button class="text-red-400" @click.prevent="removeQuestion(questionIndex)">Supprimer la question</button>
+                    <button v-if="quiz.questions.length > 1" class="text-red-400" @click.prevent="removeQuestion(questionIndex)">Supprimer la question</button>
+                    <button v-else class="text-red-200" disabled @click.prevent="removeQuestion(questionIndex)">Supprimer la question</button>
                 </div>
                 <input type="text" :id="'question-' + questionIndex" v-model="question.label" class="border-2 border-solid border-gray-200 p-2 rounded-md" placeholder="Ecrire ici">
+
+                <hr class="my-4">
 
                 <div v-for="(answer, answerIndex) in question.answers" :key="answerIndex" class="flex flex-col items-start gap-2">
                     <div class="flex flex-row justify-between w-full">
                         <label :for="'answer-' + questionIndex + '-' + answerIndex">Réponse {{ answerIndex + 1 }}</label>
-                        <button class="text-red-400" @click.prevent="removeAnswer(questionIndex, answerIndex)">Supprimer la réponse</button>
+                        <button v-if="quiz.questions[questionIndex].answers.length > 2" class="text-red-400" @click.prevent="removeAnswer(questionIndex, answerIndex)">Supprimer la réponse</button>
+                        <button v-else disabled class="text-red-200" @click.prevent="removeAnswer(questionIndex, answerIndex)">Supprimer la réponse</button>
                     </div>
                     <input type="text" :id="'answer-' + questionIndex + '-' + answerIndex" v-model="answer.label" class="border-2 border-solid border-gray-200 p-2 w-full rounded-md" placeholder="Ecrire ici">
-                    <div class="flex gap-4 mb-4">
+                    <!-- <div class="flex gap-4 mb-4">
                         <input type="radio" :name="'isCorrect' + questionIndex" :id="'isCorrect' + questionIndex">
                         <label for="isCorrect">Réponse correcte</label>
+                    </div> -->
+                    
+                    
+                    <div class="flex gap-4 mb-4" v-if="answer.isCorrect">
+                        <label :for="'isCorrect-' + questionIndex + '-' + answerIndex">Réponse correcte</label>
+                        <input type="radio" :name="'isCorrect-' + questionIndex" :id="'isCorrect-' + questionIndex + '-' + answerIndex" v-model="quiz.questions[questionIndex].correctAnswer" checked>
+                    </div>
+                    <div class="flex gap-4 mb-4" v-else>
+                        <label :for="'isCorrect-' + questionIndex + '-' + answerIndex">Réponse correcte</label>
+                        <input type="radio" :name="'isCorrect-' + questionIndex" :id="'isCorrect-' + questionIndex + '-' + answerIndex" v-model="quiz.questions[questionIndex].correctAnswer">
                     </div>
                 </div>
 
-                <button class="flex text-gray-400 mt-2" @click.prevent="addAnswer(questionIndex)">+ Ajouter une réponse</button>
+                <button v-if="quiz.questions[questionIndex].answers.length < 4" class="flex text-gray-400 mt-2" @click.prevent="addAnswer(questionIndex)">+ Ajouter une réponse</button>
             </div>
         </div>
 
@@ -36,7 +50,7 @@
             </NuxtLink>
 
             <button
-                v-if="quiz.questions.length < 4"
+                v-if="quiz.questions.length < 10"
                 @click.prevent="addQuestion"
                 class="bg-yellow-400 p-6 rounded-lg h-8 w-96 flex items-center justify-center">
                 Ajouter une question
@@ -67,7 +81,9 @@ const quiz = ref({
     questions: [
         {
             label: '',
+            correctAnswer: null,
             answers: [
+                { label: '', isCorrect: "on" },
                 { label: '', isCorrect: false },
             ]
         }
@@ -78,25 +94,24 @@ const addQuestion = () => {
     quiz.value.questions.push({
         label: '',
         answers: [
+            { label: '', isCorrect: "on" },
             { label: '', isCorrect: false },
         ]
     }); 
 };
 
 const addAnswer = (questionIndex) => {
-    if (quiz.value.questions[questionIndex].answers.length < 4) {
-        quiz.value.questions[questionIndex].answers.push({ label: '', isCorrect: false });
-    }
+    quiz.value.questions[questionIndex].answers.push({ label: '', isCorrect: false });
 };
 
 const removeQuestion = (questionIndex) => {
-    quiz.value.questions.splice(questionIndex, 1);
+    if (quiz.value.questions.length > 1) {
+        quiz.value.questions.splice(questionIndex, 1);
+    }
 };
 
 const removeAnswer = (questionIndex, answerIndex) => {
-    if (quiz.value.questions[questionIndex].answers.length > 1) {
-        quiz.value.questions[questionIndex].answers.splice(answerIndex, 1);
-    }
+    quiz.value.questions[questionIndex].answers.splice(answerIndex, 1);
 };
 
 const saveQuiz = () => {
